dc4193a110a9dd4cd4957c438a2fcf710d695ad6
kevmoo <kevmoo@j832.com>
2026-02-28 22:06:27 -0800

    Add mac-local.zsh: Modular Mac-specific environment overrides

---
+++ .zshrc.d/mac-local.zsh
@@ -0,0 +1,22 @@
+# ~/.zshrc.d/mac-local.zsh
+
+# Mac-specific PATH additions (Zsh unique array handles deduplication)
+typeset -U path
+path=(
+  "$HOME/.jetski/jetski/bin"
+  "$HOME/.pub-cache/bin"
+  "$HOME/bin"
+  "$HOME/github/depot_tools"
+  "$HOME/github/flutter/bin"
+  "$HOME/.cargo/bin"
+  "/opt/homebrew/opt/node@22/bin"
+  $path
+)
+
+# Environment Variables
+export EDITOR="subl -w"
+export RBE=1
+export RBE_cache_dir='/tmp/rbe'
+
+# Increase open file descriptor limit
+ulimit -n 10240
