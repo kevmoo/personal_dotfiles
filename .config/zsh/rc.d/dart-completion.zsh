55272ca6cd8b635d6b4edbde40c35e79637893a9
kevmoo <kevmoo@google.com>
2026-03-20 23:10:23 -0700

    jaspr cli completion crazy

---
+++ .config/zsh/rc.d/dart-completion.zsh
@@ -0,0 +1,4 @@
+## [Completion]
+## Completion scripts setup. Remove the following line to uninstall
+[[ -f /Users/kevmoo/.dart-cli-completion/zsh-config.zsh ]] && . /Users/kevmoo/.dart-cli-completion/zsh-config.zsh || true
+## [/Completion]
