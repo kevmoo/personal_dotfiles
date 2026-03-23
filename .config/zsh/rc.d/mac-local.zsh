f65965fa4cd8427cb4679eab429890428afae4b3
kevmoo <kevmoo@google.com>
2026-03-23 12:50:33 -0700

    remove python bits

--- .config/zsh/rc.d/mac-local.zsh
+++ .config/zsh/rc.d/mac-local.zsh
@@ -3,13 +3,11 @@
 # Mac-specific PATH additions
 candidates+=(
   "/opt/homebrew/share/google-cloud-sdk/bin"
-  "/opt/homebrew/opt/python@3.12/bin"
   "$HOME/.jetski/jetski/bin"
   "$HOME/.cargo/bin"
 )
 
 # Environment Variables
-export CLOUDSDK_PYTHON="/opt/homebrew/opt/python@3.12/bin/python3.12"
 export EDITOR="code --wait"
 export RBE=1
 export RBE_cache_dir='/tmp/rbe'
