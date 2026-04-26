4ac76170df0053b1a0e071b9360437ff2befcb15
kevmoo <kevmoo@j832.com>
2026-04-25 23:21:53 -0700

    Linux (Bluefin-DX): Sanitize hardcoded paths and DRY up Brewfiles after rebase

--- .config/zsh/rc.d/dart-completion.zsh
+++ .config/zsh/rc.d/dart-completion.zsh
@@ -1,4 +1,4 @@
 ## [Completion]
 ## Completion scripts setup. Remove the following line to uninstall
-[[ -f /Users/kevmoo/.dart-cli-completion/zsh-config.zsh ]] && . /Users/kevmoo/.dart-cli-completion/zsh-config.zsh || true
+[[ -f $HOME/.dart-cli-completion/zsh-config.zsh ]] && . $HOME/.dart-cli-completion/zsh-config.zsh || true
 ## [/Completion]
